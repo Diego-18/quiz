5c380893b639d9f845a3bd91c909e985e4f3190c
Diego Chavez <ingdiegochavez18@gmail.com>
2021-11-06 05:14:38 +0100

    core/initial-commit

---
+++ data/data.js
@@ -0,0 +1,48 @@
+export const data = [
+
+    {
+        question: '¿Cual de las siguientes opciones no es un editor de código?',
+        choices: [
+            'vim',
+            'vscode',
+            'sublime text',
+            'word'
+        ],
+        answer: 'word'
+    },
+
+    {
+        question: '¿Que lenguaje no es orientado a objetos?',
+        choices: [
+            'java',
+            'Haskell',
+            'c++',
+            'Python'
+        ],
+        answer: 'Haskell'
+    },
+
+    {
+        question: '¿Que lenguaje no sirve para estilizar sitios web?',
+        choices: [
+            'stylus',
+            'less',
+            'sass',
+            'PostCSS',
+            'NextCSS'
+        ],
+        answer: 'NextCSS'
+    }, 
+
+    {
+        question: '¿Que framework front-end no es capaz de crear elementos reactivos?',
+        choices: [
+            'react',
+            'vue',
+            'angular',
+            'bootstrap'
+        ],
+        answer: 'bootstrap'
+    }, 
+
+]
